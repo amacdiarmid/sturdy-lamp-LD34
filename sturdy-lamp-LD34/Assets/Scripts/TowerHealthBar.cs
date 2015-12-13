b09013298135fe0c99219806f895658c4df0a037
amacdiarmid <amacdiarmid@hotmail.co.uk>
2015-12-13 14:21:11 +0000

    health bars

---
+++ sturdy-lamp-LD34/Assets/Scripts/TowerHealthBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TowerHealthBar : MonoBehaviour
+{
+
+	public RectTransform GreenBar;
+	Tower thisTower;
+
+	// Use this for initialization
+	void Start()
+	{
+		thisTower = this.gameObject.GetComponent<Tower>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		GreenBar.localScale = new Vector3((thisTower.hp / thisTower.maxHP), 1, 1);
+	}
+}
